dd67b629264d0826c874e34a731cf309c80fd0b3
Abhishek K <abhishekakade@gmail.com>
2019-05-01 05:52:24 +0530

    update: connected to db

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// need something to connect to the database
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      // you get deprecationWarning if not used this
+      useNewUrlParser: true
+    });
+
+    console.log("MongoDB Connected...");
+
+  } catch (err) {
+    console.error(err.message);
+
+    // Exit process with failure
+    process.exit(1);
+
+  }
+}
+
+module.exports = connectDB;
